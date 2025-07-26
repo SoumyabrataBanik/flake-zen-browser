49a7e313b58d4899dc1f0a838cab2f9ec35dbb31
SoumyabrataBanik <soumyabratabanik46@gmail.com>
2025-07-26 12:15:13 +0530

    Update to v1.14.7

--- releases.nix
+++ releases.nix
@@ -1,16 +1,18 @@
+let
+  version = "1.14.7b";
+in
 {
-
-  version = "1.14.6b";
+  inherit version;
 
   sources = {
     x86_64-linux = {
-      url = "https://github.com/zen-browser/desktop/releases/download/1.14.6b/zen.linux-x86_64.tar.xz";
-      sha256 = "sha256:37c7ba78e6b7fd6c2a6988f23450f29867870e72ec93a713c3b36ee65c0443d2";
+      url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.xz";
+      sha256 = "sha256:9ec98f3925f045c9f2776ab57d2639a193e1acd0cb9e1692747786f9a815a98d";
     };
 
     aarch64-linux = {
-      url = "https://github.com/zen-browser/desktop/releases/download/1.14.6b/zen.linux-aarch64.tar.xz";
-      sha256 = "sha256:52d67013b3f4de2c2b70c849e748216e62db0b8fe5e5adf91b282cf483a6ae35";
+      url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-aarch64.tar.xz";
+      sha256 = "sha256:23c084b8a7d05bd6adb87c81f79a1b1d3929025c49b87729024f3b6bfbbd98df";
     };
   };
 }
