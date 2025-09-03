01ad2547577d8c00daf80199b501bb83e891f1ef
SoumyabrataBanik <soumyabratabanik46@gmail.com>
2025-09-03 21:03:40 +0530

    update to v1.15.3b

--- releases.nix
+++ releases.nix
@@ -1,5 +1,5 @@
 let
-  version = "1.15.2b";
+  version = "1.15.3b";
 in
 {
   inherit version;
@@ -7,12 +7,12 @@ in
   sources = {
     x86_64-linux = {
       url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.xz";
-      sha256 = "sha256:d035d4585d5d2811b1850b2e95af5e4ee0c30df6911c560fe5b52408a0110402";
+      sha256 = "sha256:8c1c22449dcd46dfa4a2dc079bda0cd8da1f7cd70b83a5b40a0b6d611d025528";
     };
 
     aarch64-linux = {
       url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-aarch64.tar.xz";
-      sha256 = "sha256:01c35497d34245faa59a9a0dc06344624c47c3118c00be69b5f2902296f282ef";
+      sha256 = "sha256:1ddc34fecf2eb4cb747440cfc5f71558b46de3f679acaef3eca496c05a674566";
     };
   };
 }
